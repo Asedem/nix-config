5c67d05e53d476d9af9b2e084768f75098a6a2f0
Asedem <asedem.games@gmail.com>
2024-04-23 09:31:25 +0200

    Terminal programms

--- device/artemis/programs.nix
+++ device/artemis/programs.nix
@@ -2,13 +2,22 @@
 
 {
   imports = [
+    # Terminal
+    ../../programs/terminal/htop.nix
+    ../../programs/terminal/java.nix
+    ../../programs/terminal/neofetch.nix
+
     # Media
     ../../programs/media/brave.nix
+    ../../programs/media/signal.nix
     ../../programs/media/spotify.nix
     ../../programs/media/discord.nix
+    ../../programs/media/tor-browser.nix
+    ../../programs/media/thunderbird.nix
 
     # Programming
     ../../programs/programming/git.nix
+    ../../programs/programming/vscode.nix
     ../../programs/programming/gitkraken.nix
     ../../programs/programming/jetbrains-toolbox.nix
 
